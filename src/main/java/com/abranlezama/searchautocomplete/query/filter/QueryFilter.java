376700eae56f55bdb4e0e076fdfa472d79e346aa
Abran Lezama <lezama.abran@gmail.com>
2024-02-11 18:53:50 -0800

    Search Query Collection Added enpoint that will receive user search requests. The requests will first go through a filter that will log them to log files for later aggregation.

---
+++ src/main/java/com/abranlezama/searchautocomplete/query/filter/QueryFilter.java
@@ -0,0 +1,31 @@
+package com.abranlezama.searchautocomplete.query.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class QueryFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String query = request.getParameter("query");
+        query = query.trim();
+        query = query.toLowerCase();
+
+        log.info(query);
+        doFilter(request, response, filterChain);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return !request.getRequestURI().contains("search") || request.getParameter("query").isBlank();
+    }
+}
