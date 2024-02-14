723ad9adce398d475fe506dce1d8525ebdb9401a
Abran Lezama <lezama.abran@gmail.com>
2024-02-14 13:27:51 -0800

    Enabled scheduling in the application

--- src/main/java/com/abranlezama/searchautocomplete/SearchAutocompleteApplication.java
+++ src/main/java/com/abranlezama/searchautocomplete/SearchAutocompleteApplication.java
@@ -2,8 +2,10 @@ package com.abranlezama.searchautocomplete;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SearchAutocompleteApplication {
 
     public static void main(String[] args) {
