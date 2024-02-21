0d05c5c25ed5ee2da33c796f7ec92804b160ecfa
Abran Lezama <lezama.abran@gmail.com>
2024-02-20 18:54:34 -0800

    Changed collection name to query_logs

--- src/main/java/com/abranlezama/searchautocomplete/queryaggregation/entity/QueryRecord.java
+++ src/main/java/com/abranlezama/searchautocomplete/queryaggregation/entity/QueryRecord.java
@@ -5,19 +5,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Document(collection = "aggregated_query_logs")
+@Document(collection = "query_logs")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@CompoundIndexes({
+        @CompoundIndex(useGeneratedName = true, unique = true, def = "{'query': 1, 'weeklyEntries.weekOf': 1}")
+})
 public class QueryRecord {
 
     @Id
     private String id;
+    @Indexed(unique = true, useGeneratedName = true)
     private String query;
     private int totalQueries;
     private List<WeeklyEntry> weeklyEntries = new ArrayList<>();
